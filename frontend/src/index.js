d879ad8febc1aa9d6212bf783dc5542a542712ee
Ali-Kh-1997 <your-email@example.com>
2026-08-20 12:51:28 +0330

    رفع خطای decorators در index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
